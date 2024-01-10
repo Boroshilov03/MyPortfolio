0923738cb7997d897edfda2febdffa1768b82a91
Mirlan Boroshilov <mika.borosh@gmail.com>
2024-01-09 23:42:03 -0800

    Added about page, however, have not done any css yet

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import './App.css'
 import NavBar from "./components/NavBarComp/NavBar.jsx";
 import MainTitle from "./components/PageComp/HomeComp/MainTitle.jsx";
+import AboutPage from './components/PageComp/HomeComp/AboutPage.jsx';
 import WorkSection from "./components/PageComp/HomeComp/WorkSection.jsx";
+
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
         <div className={"flex flex-col gap-7 p-5"}>
             <NavBar />
             <MainTitle />
+            <AboutPage />
             <WorkSection />
         </div>
     )
---
+++ src/components/PageComp/HomeComp/AboutPage.jsx
@@ -0,0 +1,40 @@
+function AboutPage() {
+    return (
+        <div className="lg:flex block items-start justify-evenly border-b-4 pb-9">
+            {/* Relevant Experience Section */}
+            <div className="lg:w-1/2 ">
+                <h1 className={"font-bold"}>Relevant Experience</h1>
+                <ul className={"mt-5 flex flex-col gap-5"}>
+                <li>DubHacks - Hackaton<br /><span className="font-light">October of 2023</span></li>
+                <li>
+                  CSULB Programming Team<br /><span className="font-light">January - June of
+                  2023</span>
+                </li>
+                <li>UCLA - Hackaton<br /><span className="font-light">April of 2023</span></li>
+                <li>CSULB - Hackaton<br /><span className="font-light">February of 2023</span></li>
+              </ul>
+            </div>
+
+            {/* About Page Section */}
+            <div className="lg:w-1/2">
+                <h1 className={"font-bold"}>About Page</h1>
+                <p className={"mt-5"}>
+                Hello 🙋
+
+                I am an undergraduate with a degree in computer science. 
+                I am passionate about using technology to solve complex problems and make a positive impact on the world🙂.
+
+                With experience as a full-stack developer or software developer, 
+                I have developed a strong foundation in programming languages such as Python, C++, JavaScript, HTML, CSS, Flask, MongoDB, and SQL. 
+                I am also interested in machine learning and AI, which is why I am currently learning it. 
+                As a creative problem solver and team player, I am committed to delivering high-quality work and exceeding expectations. 
+                I am always eager to learn, connect with other technologies and tools, and explore new opportunities for growth and development. 
+                </p>
+                <button className="mt-5">Download CV</button>
+            </div>
+        </div>
+    );
+}
+
+ 
+export default AboutPage;
