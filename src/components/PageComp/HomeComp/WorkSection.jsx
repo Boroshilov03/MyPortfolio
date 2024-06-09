2240b3cc3d0768e30b5991696694a81a465eaa9d
Boroshilov03 <boroshilov.mirlan@gmail.com>
2024-06-09 14:38:37 -0700

    new projects added

--- src/components/PageComp/HomeComp/WorkSection.jsx
+++ src/components/PageComp/HomeComp/WorkSection.jsx
@@ -1,12 +1,16 @@
-import BudgetPal from "/src/assets/BudgetP/Sign Up.png";
-import J4J from "/src/assets/Projects/J4J-Main.png";
+import BudgetPal from "/src/assets/Projects/BudjetPal.png";
+import J4J from "/src/assets/Projects/J4Jpic2.png";
 import MovieLand from "/src/assets/Projects/MovieLand.png";
 import QuoteGen from "/src/assets/Projects/QuoteGen.png";
 import GPT from "/src/assets/Projects/GPT-3.png";
-import Music from "/src/assets/Projects/Music.png";
+import FoodTruck from "/src/assets/Projects/FoodTruck1.png";
 
 const ProjectCard = ({ dataItem }) => {
   const { link, title, website } = dataItem;
+
+  const imageClass = title === "MovieLand"
+    ? "object-cover hover:scale-105 transform transition-transform duration-500 w-full h-full rounded-lg"
+    : "object-contain hover:scale-105 transform transition-transform duration-500 w-full h-full rounded-lg";
 
   return (
     <li className="lg:w-[47%] mb-10 relative">
@@ -17,16 +21,11 @@ const ProjectCard = ({ dataItem }) => {
         style={{ position: "relative", display: "block" }}
       >
         <img
-          className={
-            "object-cover hover:scale-95 transform transition-transform duration-500 w-full h-full rounded-lg"
-          }
+          className={imageClass}
           src={link}
           alt={title}
           style={{ height: "450px" }}
         />
-
-        {/* <i className="text-7xl text-gray-300  absolute bi bi-arrow-up-right-circle top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></i>{" "} */}
-        {/* Position the arrow in the middle */}
         <h2 className="lg:text-[40px] text-[35px] font-light">{title}</h2>
       </a>
     </li>
@@ -35,6 +34,16 @@ const ProjectCard = ({ dataItem }) => {
 
 function WorkSection() {
   const data = [
+    {
+      title: "GPT3- UI/UX",
+      website: "https://github.com/Boroshilov03/GPT3-UI-UX",
+      link: GPT,
+    },
+    {
+      title: "Food Truck Finder",
+      website: "https://github.com/Boroshilov03/GPT3-UI-UX",
+      link: FoodTruck,
+    },
     {
       title: "BudgetPal",
       website: "https://github.com/Boroshilov03/BudgetPal",
@@ -55,16 +64,6 @@ function WorkSection() {
       website: "https://github.com/Boroshilov03/MovieLand",
       link: MovieLand,
     },
-    {
-      title: "GPT3- UI/UX",
-      website: "https://github.com/Boroshilov03/GPT3-UI-UX",
-      link: GPT,
-    },
-    // {
-    //   title: "Music",
-    //   website: "https://github.com/Boroshilov03/MovieLand",
-    //   link: Music
-    // }
   ];
 
   return (
